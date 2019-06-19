a37cf20a1af747d318533806c3e61470e3286469
anatol-blackforest <anatolblackforest@gmail.com>
2019-06-19 12:03:53 +0300

    FINAL 3

--- controllers/getCtrl.js
+++ controllers/getCtrl.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
         const db = await pool.connect()
         const data = await db.query('SELECT * FROM items ORDER BY id DESC')
         db.release()
-        res.status(200).render('index', {items: data.rows})
+        return res.status(200).render('index', {items: data.rows})
     }catch(err) {
         console.error('Unable to connect to the database:', err);
         return res.status(500).json({error: 500});
